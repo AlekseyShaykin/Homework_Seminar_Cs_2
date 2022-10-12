90995d4f0fbffa7d239da6b9cc24da37450712c8
Aleksey <alekseyshaykin2311@gmail.com>
2022-10-13 06:32:56 +0700

    Task #27 has done

---
+++ Seminar_3_Task_27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// вводим число
+int GetValue()
+{
+    Console.Write("Input number ");
+    int result = Int32.Parse(Console.ReadLine());
+    return result;
+}
+
+
+int SumElement(int x)
+{
+    int sum = 0;
+    int o = 0;
+
+    while (x != 0)
+    {
+        o = x % 10;
+        x = x / 10;
+        sum = sum + o;
+    }
+  
+    return sum;
+}
+
+int n = GetValue();
+int result = SumElement(n);
+Console.WriteLine("The sum of elements = " + result);
+
